e82a6dc78ccb191a147d6ccfdc7b674fb75e2ca1
Satpal <deepsatpal>
2023-02-28 15:05:44 +0530

    changes

--- auths/email.py
+++ auths/email.py
@@ -5,23 +5,23 @@ from djoser import utils
 from djoser.conf import settings
 
 
-class ActivationEmail(BaseEmailMessage):
-    print("HERE")
-    template_name = "email/activation.html"
+# class ActivationEmail(BaseEmailMessage):
+#     print("HERE")
+#     template_name = "email/activation.html"
 
-    def get_context_data(self):
-        # ActivationEmail can be deleted
-        context = super().get_context_data()
-        print("Context ActivationEmail" , context)
-        user = context.get("user")
-        context["uid"] = utils.encode_uid(user.pk)
-        context["token"] = default_token_generator.make_token(user)
-        context["url"] = settings.ACTIVATION_URL.format(**context)
-        return context
+#     def get_context_data(self):
+#         # ActivationEmail can be deleted
+#         context = super().get_context_data()
+#         print("Context ActivationEmail" , context)
+#         user = context.get("user")
+#         context["uid"] = utils.encode_uid(user.pk)
+#         context["token"] = default_token_generator.make_token(user)
+#         context["url"] = settings.ACTIVATION_URL.format(**context)
+#         return context
 
 
-class ConfirmationEmail(BaseEmailMessage):
-    template_name = "email/confirmation.html"
+# class ConfirmationEmail(BaseEmailMessage):
+#     template_name = "email/confirmation.html"
 
 
 class PasswordResetEmail(BaseEmailMessage):
